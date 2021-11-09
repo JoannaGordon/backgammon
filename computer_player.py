3b4f25395eed05046f3560732c3cd353c142cfdb
JoannaGordon <jhhg@hotmail.co.uk>
2021-11-09 23:35:22 +0000

    Started to create computer player

---
+++ computer_player.py
@@ -0,0 +1,24 @@
+import pandas
+
+dice = [1,2]
+board = [-2,0,0,0,0,0,-3,0,0,-5,5,0,0,3,0,0,0,0,0,2]
+location = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
+player = [1]
+
+computer1(board,dice,player):
+    #loop:
+    #from locations of neg/positive) 
+    
+
+# input of current board 
+# input of dice roll 
+
+# possible moves 
+# to calculate possible moves search to left or right depending on player 
+# find out where there are vunerabilities,
+# where there are blots, 
+# where there are blank spaces, 
+# where are there spaces with your pieces 
+ 
+#Decide on two moves depending on information above
+
