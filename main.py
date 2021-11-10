f086614ba91a78037022828a6fabe3c9d98077cc
Sam James <samajames@outlook.com>
2021-11-10 15:15:07 +0000

    imported board and player classes and steup basic structure to the game

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-
-
+from board_class import *
+from player_classes import *
 
 def main():
-    print('Hello world!')
+    board = Board()
     
+    player1 = Human()
+    player2 = Human()
+    
+    players = [player1, player2]
+    players_turn = 0
+    
+    while not win():
+        board.print_board()
+        
+        players[players_turn].set_move()
+        
+        board.make_move(players[players_turn].counter_moves)
+        
+        players_turn = (players_turn + 1) % 2
+        
+
+def win():
+    return False
     
 if __name__ == "__main__":
-    main():
+    main()
